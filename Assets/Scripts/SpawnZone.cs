b3f98807a40af92b08d0c39e57224b83776e6b09
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-10-28 10:21:18 +1100

    Initial

---
+++ Assets/Scripts/SpawnZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SpawnZone : MonoBehaviour
+{
+    public Vector3 spawnPoint
+    {
+        get
+        {
+            return Random.insideUnitSphere * 5f;
+        }
+    }
+}
