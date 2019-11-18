86c8cd2b5bf8a360bbb192b080c493e82dfc44e9
Helmi Amsani <48036579+helmiamsani@users.noreply.github.com>
2019-11-18 12:00:48 +1100

    boom

--- Assets/Scripts/Fabricating Shapes/ShapeFactory.cs
+++ Assets/Scripts/Fabricating Shapes/ShapeFactory.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [CreateAssetMenu]
 public class ShapeFactory : ScriptableObject
@@ -9,12 +10,33 @@ public class ShapeFactory : ScriptableObject
     [SerializeField]
     private Material[] materials;
 
+    List<Shape>[] pools;
+
+    [SerializeField]
+    bool recycle;
+
     public Shape Get(int shapeId = 0, int materialId = 0)
     {
-        Shape instance = Instantiate(prefabs[shapeId]);
-        instance.ShapeId = shapeId;
+        Shape instance;
+        if (recycle)
+        {
+            if(pools == null)
+            {
+                CreatePools();
+            }
+            List<Shape> pool = pools[shapeId];
+            int lastIndex = pool.Count - 1;
+            instance = pool[lastIndex];
+            instance.gameObject.SetActive(true);
+            pool.RemoveAt(lastIndex);
+        }
+        else
+        {
+            instance = Instantiate(prefabs[shapeId]);
+            instance.ShapeId = shapeId;
+        }
+
         instance.SetMaterial(materials[materialId], materialId);
-        return instance;
     }
 
     public Shape GetRandom()
@@ -23,6 +45,15 @@ public class ShapeFactory : ScriptableObject
             Random.Range(0, prefabs.Length), 
             Random.Range(0, materials.Length)
         );
+    }
+
+    void CreatePools()
+    {
+        pools = new List<Shape>[prefabs.Length];
+        for (int i = 0; i < pools.Length; i++)
+        {
+            pools[i] = new List<Shape>();
+        }
     }
 }
 
